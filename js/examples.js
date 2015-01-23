e2cf29c54f75d94637335b1d39ec6ece2add17cd
Lennon Day-Reynolds <lennon@rcoder.net>
2015-01-23 10:20:14 -0800

    allow loading of example code into scratchpad

---
+++ js/examples.js
@@ -0,0 +1,4 @@
+const EXAMPLES = {
+  "hello":"Tools.renderTweets(['Hello, world!']);",
+  "markov":'var corpus = $("#markov textarea").val();\noutput = Tools.markovTrainAndGenerate(corpus);\nTools.renderTweets(output);',
+}
