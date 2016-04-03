2f9a8d3d10c6caa8c2a10c438784ec93b74136c2
Greg Borenstein <greg.borenstein@gmail.com>
2016-04-03 12:59:20 -0700

    add data usage example, copy ide to be at index

--- js/examples.js
+++ js/examples.js
@@ -2,5 +2,7 @@ const EXAMPLES = {
   "hello":"js/examples/hello.js",
   "markov":"js/examples/markov.js",
   "two headlines (part of speech)" : "js/examples/pos_two_headlines.js",
-  "fart/art (fetch page and replace word)" : "js/examples/fetch_fart_art.js"
+  "fart/art (fetch page and replace word)" : "js/examples/fetch_fart_art.js",
+  "trivial pursuit (DATA example)" : "js/examples/data_example.js"
+
 }
